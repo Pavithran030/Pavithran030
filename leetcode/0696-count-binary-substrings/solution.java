09a8336d45b60229a71fd1e3b0465b0f0cb0343f
Pavithran G <147511371+Pavithran030@users.noreply.github.com>
2026-02-19 18:27:05 +0000

    🧩 [LeetCode Sync] Runtime - 11 ms (54.07%), Memory - 46.5 MB (24.73%)

---
+++ leetcode/0696-count-binary-substrings/solution.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int countBinarySubstrings(String s) {
+        int pre = 0;
+        int res = 0;
+        int cur = 1;
+
+        for(int i=1;i<s.length();i++){
+            if(s.charAt(i) != s.charAt(i-1)){
+                res+=Math.min(cur,pre);
+                pre=cur;
+                cur=1;
+            }
+            else{
+                cur+=1;                
+            }
+        }
+        res+=Math.min(cur,pre);
+        return res;
+    }
+}
