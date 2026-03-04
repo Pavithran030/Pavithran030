52a9601ae24085d558f2cd4a2114725abde39665
Pavithran G <147511371+Pavithran030@users.noreply.github.com>
2026-03-04 09:40:43 +0000

    🧩 [LeetCode Sync] Runtime - 2 ms (97.49%), Memory - 47 MB (85.27%)

---
+++ leetcode/1704-special-positions-in-a-binary-matrix/solution.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSpecial(int[][] mat) {
+
+        int m = mat.length,n = mat[0].length;
+
+        int[] row=new int[m];
+        int[] col=new int[n];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1){
+                    row[i]++;
+                    col[j]++;
+                }
+            }
+        }
+
+        int re=0;
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(mat[i][j]==1 && row[i]==1 && col[j]==1){
+                    re++;
+                }
+            }
+        }
+        return re;
+    }
+}
