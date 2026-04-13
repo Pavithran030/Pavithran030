1654fb5d227810820dacd970eeb5c48f009a4f79
Pavithran G <147511371+Pavithran030@users.noreply.github.com>
2026-04-13 12:52:08 +0000

    🧩 [LeetCode Sync] Runtime - 0 ms (100.00%), Memory - 45 MB (23.61%)

--- leetcode/1975-minimum-distance-to-the-target-element/solution.java
+++ leetcode/1975-minimum-distance-to-the-target-element/solution.java
@@ -1,10 +1,14 @@
 class Solution {
     public int getMinDistance(int[] nums, int target, int start) {
-        int ans=Integer.MAX_VALUE;
+        
+        int min=Integer.MAX_VALUE;
         for(int i=0;i<nums.length;i++){
-            if(nums[i]==target) 
-                ans=Math.min(ans,Math.abs(i-start));
-        }  
-        return ans;      
+            if(nums[i]==target){
+                int dif=Math.abs(i-start);
+                min=Math.min(min,dif);
+            }
+        }
+        return min;
+        
     }
 }
