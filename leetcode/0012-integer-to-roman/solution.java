5d3759ac38ec1b067df3a83f5d53486a2c6fa5e5
Pavithran G <147511371+Pavithran030@users.noreply.github.com>
2026-04-19 17:21:03 +0000

    🧩 [LeetCode Sync] Runtime - 3 ms (99.86%), Memory - 46.2 MB (89.06%)

---
+++ leetcode/0012-integer-to-roman/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] values = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] symbols = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        StringBuilder re = new StringBuilder();
+
+        for(int i=0;i<values.length;i++){
+            while(num>=values[i]){
+                re.append(symbols[i]);
+                num-=values[i];
+            }
+        }
+        return re.toString();        
+    }
+}
